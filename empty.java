d4934736ffe38bd3ee04b0a4428cbaa13385b672
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-03-30 18:11:36 -0300

    Add stack implementation for Resident Evil titles
    Implement a stack to manage Resident Evil titles and print status.

---
+++ empty.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class pilha {
+    public static void main(String[] args) {
+
+        Stack<String> ResidentEvil = new Stack<>();
+
+        ResidentEvil.push("Biohazard");
+        ResidentEvil.push("Reqiem");
+
+        System.out.println("topo " + ResidentEvil.pop());
+
+        System.out.println(ResidentEvil.empty()?"vazio":"com elemento");
+        
+        //Printar se esta vazia ou nao.
+
+        for (String r:ResidentEvil) {
+            System.out.println(r);
+        }
+    }
+}
