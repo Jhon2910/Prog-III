6e26acb70ebbe4cfa975498153ab872c1cf9d6fb
Jonathan Alexandre Rodrigues Alves <91800109+Jhon2910@users.noreply.github.com>
2026-03-30 21:18:03 -0300

    Update clear.java

--- Pilha/clear.java
+++ Pilha/clear.java
@@ -1,1 +1,24 @@
+import java.util.Stack;
 
+public class pilha {
+    public static void main(String[] args) {
+
+        Stack<String> ResidentEvil = new Stack<>();
+
+        ResidentEvil.push("Biohazard");
+        ResidentEvil.push("Requiem");
+
+        System.out.println("topo " + ResidentEvil.pop());
+
+        ResidentEvil.clear();
+
+        //Limpar a pilha
+
+        System.out.println(ResidentEvil.empty()?"vazio":"com elemento");
+
+
+        for (String r:ResidentEvil) {
+            System.out.println(r);
+        }
+    }
+}
